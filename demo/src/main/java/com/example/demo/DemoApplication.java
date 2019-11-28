3154b4710cbcdd4fb16dbc8dea75c437fb22e3eb
Albert <765841240@qq.com>
2019-11-28 10:38:05 +0800

    测试用例创建

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        System.out.println(Runtime.getRuntime().totalMemory());
+        System.out.println(Runtime.getRuntime().maxMemory());
+        System.out.println(Runtime.getRuntime().freeMemory());
+        //SpringApplication.run(DemoApplication.class, args);
     }
 
 }
